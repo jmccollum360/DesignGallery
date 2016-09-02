7261b02dc50bd6367a3ace3605b89a2954e5439b
JASON <jmccollum@cimlbr.com>
2016-09-02 15:51:40 -0500

    added compass dependency for sass and browserify

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
+    compass = require('gulp-compass'),
+    browserify = require('browserify'),
     webserver = require('gulp-webserver');
 
 var sassSources = ('components/sass/*.scss');
@@ -10,15 +12,20 @@ var jsSources = [
     'components/scripts/bootstrap.min.js'
   ]
     
-     gulp.task('sass', function(){
+     gulp.task('compass', function(){
      return gulp.src(sassSources)
-      .pipe(sass())
+      .pipe(compass({
+        sass: 'components/sass',
+        image: 'builds/dev/images',
+        style: 'expanded'
+      }))
       .pipe(gulp.dest('builds/dev/css'))
     });
 
     gulp.task('js', function(){
      return gulp.src(jsSources)
       .pipe(concat('scripts.js'))
+      .pipe(browserify())
       .pipe(gulp.dest('builds/dev/js'))
     });
 
@@ -31,8 +38,8 @@ var jsSources = [
     });
 
     gulp.task('watch', function(){
-      gulp.watch('sassSources', ['sass']);
+      gulp.watch('components/sass/*.scss', ['compass']);
       gulp.watch('jsSources', ['js']);
     });
 
-    gulp.task('default',['sass', 'js', 'webserver']);
+    gulp.task('default',['compass', 'js', 'webserver']);
