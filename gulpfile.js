aae25772840c79cba99c275cad84c043d4998750
JASON <jmccollum@cimlbr.com>
2016-09-06 15:19:20 -0500

    updated files for build

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,6 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
-    compass = require('gulp-compass'),
-    browserify = require('browserify'),
-    connect = require('gulp-connect'),
     webserver = require('gulp-webserver');
 
 var sassSources = ('components/sass/*.scss');
@@ -12,41 +9,36 @@ var jsSources = [
     'components/scripts/jquery-3.1.0.min',
     'components/scripts/bootstrap.min.js'
   ];
+var build =('builds/dev');  
     
-     gulp.task('compass', function(){
+     gulp.task('sass', function(){
      return gulp.src(sassSources)
-      .pipe(compass({
-        sass: 'components/sass',
-        image: 'builds/dev/images',
-        style: 'expanded'
-      }))
+      .pipe(sass())
       .pipe(gulp.dest('builds/dev/css'))
-      .pipe(connect.reload())
     });
 
     gulp.task('js', function(){
      return gulp.src(jsSources)
       .pipe(concat('scripts.js'))
       .pipe(gulp.dest('builds/dev/js'))
-      .pipe(connect.reload())
     });
 
     gulp.task('watch', function(){
-      gulp.watch('components/sass/*.scss', ['compass']);
+      gulp.watch('components/sass/*.scss', ['sass']);
       gulp.watch('jsSources', ['js']);
       gulp.watch('builds/dev/*.html', ['html']);
     });
 
-    gulp.task('connect', function(){
-      connect.server({
-        root: 'builds/dev/',
-        livereload: true
-      });
+    gulp.task('webserver', function(){
+      gulp.src('builds/dev')
+      .pipe(webserver({
+        livereload: true,
+        open: true
+      }));
     });
 
     gulp.task('html', function(){
       gulp.src('builds/dev/*.html')
-        .pipe(connect.reload());
-    });
+   });
 
-    gulp.task('default',['js', 'compass', 'watch', 'watch', 'connect']);
+    gulp.task('default',['js', 'html', 'watch', 'sass', 'webserver']);
