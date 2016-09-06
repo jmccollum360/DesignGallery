fcc8acffff783e4926f6a958dbc47fc15037f02e
JASON <jmccollum@cimlbr.com>
2016-09-06 14:57:21 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,7 @@ var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
-    compass = require('gulp-compass'),
-    gutil = require('gulp-util'),
     cssnano = require('cssnano'),
-    autoprefixer = require('autoprefixer'),
     webserver = require('gulp-webserver');
 
 var sassSrc = ('components/sass/*.scss');
