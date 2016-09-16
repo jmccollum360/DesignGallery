6ad35a0b2c95fb521441c853d99045340550de41
JASON <jmccollum@cimlbr.com>
2016-09-16 10:31:42 -0500

    updated changes to index, gulpfile, style

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,41 @@
+/******required variables....will also need to write the uglify 
+function once ready to put in production******/
+
 var gulp = require('gulp'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify'),
     sass = require('gulp-sass'),
     webserver = require('gulp-webserver');
 
-var sassSources = ('components/sass/*.scss');
+// Path for all sass/scss files before conversion to single css sheet
+var sassSources = ('components/sass/*.scss');   
+
+// Path for required scripts in array to run in right sequence before concatenating to single script
 var jsSources = (['components/scripts/jquery-3.1.0.min.js', 'components/scripts/bootstrap.min.js']);
+
+// Path to development to make functions cleaner
 var build =('builds/dev');  
     
-     gulp.task('sass', function(){
+    
+     gulp.task('sass', function(){    //Takes all scss components and converts to css placing in dev folder
      return gulp.src(sassSources)
       .pipe(sass())
       .pipe(gulp.dest('builds/dev/css'))
     });
 
-    gulp.task('js', function(){
+    gulp.task('js', function(){       //takes jquery and bootstrap required js in order and concatenates to dev folder
      return gulp.src(jsSources)
       .pipe(concat('scripts.js'))
       .pipe(gulp.dest('builds/dev/js'))
     });
 
-    gulp.task('watch', function(){
+    gulp.task('watch', function(){    //Anytime html, scss, or js files change run node
       gulp.watch('components/sass/*.scss', ['sass']);
       gulp.watch('jsSources', ['js']);
       gulp.watch('builds/dev/*.html', ['html']);
     });
 
-    gulp.task('webserver', function(){
+    gulp.task('webserver', function(){  //Function to live reload after saving any changes
       gulp.src('builds/dev')
       .pipe(webserver({
         livereload: true,
@@ -34,8 +43,4 @@ var build =('builds/dev');
       }));
     });
 
-    gulp.task('html', function(){
-      gulp.src('builds/dev/*.html')
-   });
-
-    gulp.task('default',['js', 'html', 'watch', 'sass', 'webserver']);
+    gulp.task('default',['js', 'watch', 'sass', 'webserver']);  //run with just gulp and goes through each function
